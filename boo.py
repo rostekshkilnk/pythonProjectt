7409f460ab0348447355c218feaa2cd05d4919c7
rostekshkilnk <rostislavskilnuk@gmail.com>
2023-02-24 14:06:45 -0500

    new

---
+++ boo.py
@@ -0,0 +1,9 @@
+class Student:
+    print('hi')
+    print('hello')
+    print('how are you')
+    print('im fine')
+    print('what are you doing')
+    print('nothing')
+    print('ok then goodbye')
+    print('goodbye')
